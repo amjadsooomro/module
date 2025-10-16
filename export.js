da5cc96504c78906053deb4cea4c96c133c9f4ed
amjad ali <you@example.com>
2025-10-15 22:52:07 -0700

    ok

---
+++ export.js
@@ -0,0 +1,20 @@
+// export var x="test";
+
+// export var abc=(name)=>{
+
+//     console.log("hello fun"+name);
+// }
+
+
+
+// export const hello = () => console.log("Hello World!");
+// hello();   // Output: Hello World!
+
+
+
+export class abc{
+
+    test(name){
+          console.log(name);
+    }
+}
---
+++ import.js
@@ -0,0 +1,11 @@
+// import {abc} from './export.js'
+
+//  var obj = new abc();
+
+// obj.test();
+
+
+import { abc } from './export.js';
+
+var obj = new abc();
+obj.test('y');  // Output: hello
